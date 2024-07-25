799a9c062ce781ec26589cf4cfc3edd03b3c7ba3
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-07-25 17:38:07 +1200

    lecture 1 to lecture 3 exercises

---
+++ l01 - l03/ex2/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    internal class Student
+    {
+        public Student(string fn, string ln, int a, string g, string d) {
+            count++;
+            FirstName = fn; LastName = ln; Age = a; Gender = g; Department = d;
+        }
+
+        public static int count = 0;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+
+        public static void PrintDetails(Student s) {
+            Console.WriteLine("Student {0} Details:", count);
+            Console.WriteLine("First Name: {0}", s.FirstName);
+            Console.WriteLine("Last Name: {0}", s.LastName);
+            Console.WriteLine("Age: {0}", s.Age);
+            Console.WriteLine("Gender: {0}", s.Gender);
+            Console.WriteLine("Department: {0}", s.Department);
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ l01 - l03/ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ex3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car v1 = new Car("Toyota", "Corolla", 2020, 4, "Sedan");
+        Car.DisplayInfo(v1);
+
+        Motorcycle v2 = new Motorcycle("Honda", "CBR600RR", 2019, 2.2);
+        Motorcycle.DisplayInfo(v2);
+
+        Car.Drive(v1);
+        Motorcycle.Ride(v2);
+    }
+}
