548e32866638923c7ef0aa192426273473f2f142
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-07-25 17:40:44 +1200

    lecture 04 ex1

---
+++ l04/ex1/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l04_ex1
+{
+    internal class Person
+    {
+        public Person(string n, int a) 
+        {
+            Name = n; Age = a;
+        }
+        public string Name {  get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ l04/ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace l04_ex1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> _people = new List<Person>()
+        {
+            new Person("Bob", 35), new Person("Charlie", 40), new Person("Anthony", 20), new Person("Timmy", 12)
+        };
+
+        var oldpeople = _people.Where(p => p.Age > 30);
+
+        Console.WriteLine("Persons older than 30:");
+        foreach (var person in oldpeople)
+        { 
+            Console.WriteLine("Name: {0}, Age: {1}", person.Name, person.Age);
+        }
+    }
+}
