ef50a6179e2a6019536f521f7aa5f9456a32cc55
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-07-25 17:42:26 +1200

    lecture 4, ex 2-4

---
+++ l04/ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace l04_ex3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>()
+        {
+            new Person("Alice", "Manager", 30),
+            new Person("Bob", "Developer", 25),
+            new Person("Charlie", "Designer", 33),
+            new Person("David", "Analyst", 26),
+            new Person("Eve", "Tester", 28)
+        };
+
+        var mappedpeople = people.Select(person => new { Name = person.Name, Role = person.Role });
+
+        Console.WriteLine("Mapping of names to roles:");
+
+        foreach (var m in mappedpeople)
+        {
+            Console.WriteLine("Name: {0}, Role: {1}", m.Name, m.Role);
+        }
+    }
+}
