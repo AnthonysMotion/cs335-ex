d517a2cb778b5052a3b888576ae03b7ddbd66de2
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-07-25 17:29:25 +1200

    l04 ex4

---
+++ l04-ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace l04_ex4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>()
+        {
+            new Person("Eve", 30),
+            new Person("David", 20),
+            new Person("Bob", 35),
+            new Person("Alice", 25),
+            new Person("Charlie", 30)
+        };
+
+        var sortedpeople = people.OrderBy(p => p.Age).ThenBy(p => p.Name);
+
+        Console.WriteLine("Sorted list of persons:");
+
+        foreach (var person in sortedpeople) { 
+            Console.WriteLine("Name: {0}, Age: {1}", person.Name, person.Age);
+        }
+    }
+}
