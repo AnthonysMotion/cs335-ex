276534ce6a931a5f8c620d1d4e97b9f86c088d0c
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-07-25 16:56:33 +1200

    l04 ex2

---
+++ l04-ex2/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l04_ex2
+{
+    internal class Employee
+    {
+        public Employee(string n, string d, double s) 
+        {
+            Name = n; Department = d; Salary = s;
+        }
+
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+    }
+}
