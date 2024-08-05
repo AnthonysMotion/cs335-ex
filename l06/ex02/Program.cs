707fe00de2b8daa01de9735de138e1032697c4a4
Anthony <120075541+AnthonysMotion@users.noreply.github.com>
2024-08-05 17:31:47 +1200

    Add files via upload

--- l06/ex02/Program.cs
+++ l06/ex02/Program.cs
@@ -8,7 +8,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        CreateStudent(new Student { FirstName = "John", LastName = "Doe", Address = "john.doe street" }); ;
+        CreateStudent(new Student { FirstName = "John", LastName = "Doe", Address = "john.doe street" });
+        GetStudents();
+        UpdateStudent(new Student { Id = 1, FirstName = "Jane", LastName = "Doe", Address = "jane.doe street" });
+        GetStudents();
+        CreateStudent(new Student { FirstName = "John", LastName = "Doe", Address = "john.doe street" });
+        GetStudents();
+        DeleteStudent(2);
+        GetStudents();
     }
 
     static void CreateStudent(Student student)
@@ -18,6 +25,49 @@ class Program
             context.Students.Add(student);
             context.SaveChanges();
             Console.WriteLine($"Student {student.FirstName} {student.LastName} created.");
+        }
+    }
+
+    static void GetStudents()
+    {
+        using (var context = new StudentDbContext())
+        {
+            var students = context.Students.ToList();
+            Console.WriteLine("Students: ");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Id} : {student.FirstName} {student.LastName} ({student.Address})");
+            }
+        }
+    }
+
+    static void UpdateStudent(Student student)
+    {
+        using (var context = new StudentDbContext())
+        {
+            var currentStudent = context.Students.FirstOrDefault(s => s.Id == student.Id);
+            if (currentStudent != null)
+            {
+                currentStudent.Id = student.Id;
+                currentStudent.FirstName = student.FirstName;
+                currentStudent.LastName = student.LastName;
+                currentStudent.Address = student.Address;
+            }
+            context.SaveChanges();
+        }
+    }
+
+    static void DeleteStudent(int id)
+    {
+        using (var context = new StudentDbContext())
+        {
+            var studentToDelete = context.Students.FirstOrDefault(e => e.Id == id);
+            if (studentToDelete != null)
+            {
+                context.Students.Remove(studentToDelete);
+            }
+            Console.WriteLine("deleted");
+            context.SaveChanges();
         }
     }
 }
